3cd4071bd7f9e8e9dfb0afa68d1b5c6616a3c264
Takashi Matsuo <tmatsuo@google.com>
2015-11-24 10:58:26 -0800

    Test refactoring, added promote_by_default = false.

---
+++ scripts/install_test_dependencies.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+# Copyright 2015 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -ex
+
+sudo apt-get update
+sudo apt-get install -y php5-cli
+
+# Install composer and php-cs-fixer
+curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
+wget http://get.sensiolabs.org/php-cs-fixer.phar -O php-cs-fixer
+sudo mv php-cs-fixer /usr/local/bin
+sudo chmod +x /usr/local/bin/php-cs-fixer
+composer install --ignore-platform-reqs
+
+# Install gcloud
+if [ ! -d ${HOME}/gcloud/google-cloud-sdk ]; then
+    mkdir -p ${HOME}/gcloud &&
+    wget https://dl.google.com/dl/cloudsdk/release/google-cloud-sdk.tar.gz --directory-prefix=${HOME}/gcloud &&
+    cd "${HOME}/gcloud" &&
+    tar xzf google-cloud-sdk.tar.gz &&
+    ./google-cloud-sdk/install.sh --usage-reporting false --path-update false --command-completion false &&
+    cd "${TRAVIS_BUILD_DIR}";
+fi
+
+# gcloud configurations
+gcloud -q components update app gsutil
+gcloud config set project ${GOOGLE_PROJECT_ID}
+gcloud config set app/promote_by_default false
