4034df3bb671d61b675cd953e86248d6b153d993
Takashi Matsuo <tmatsuo@google.com>
2015-11-24 12:30:53 -0800

    Stop tests when CLOUDSDK_ACTIVE_CONFIG_NAME envvar is not set.

--- scripts/install_test_dependencies.sh
+++ scripts/install_test_dependencies.sh
@@ -18,6 +18,11 @@ set -ex
 sudo apt-get update
 sudo apt-get install -y php5-cli
 
+if [ -z "${CLOUDSDK_ACTIVE_CONFIG_NAME}" ]; then
+    echo "You need to set CLOUDSDK_ACTIVE_CONFIG_NAME envvar."
+    exit 1
+fi
+
 # Install composer and php-cs-fixer
 curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
 wget http://get.sensiolabs.org/php-cs-fixer.phar -O php-cs-fixer
