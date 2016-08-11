100c022780be0bfdf9233cdb07cc97d872479582
Takashi Matsuo <tmatsuo@google.com>
2016-08-10 22:20:43 -0700

    Put php in the PATH when invoking the script.

--- php-nginx/entrypoint.sh
+++ php-nginx/entrypoint.sh
@@ -116,7 +116,7 @@ ${PHP_DIR}/bin/php /whitelist_functions.php
 
 if [ -f "${APP_DIR}/composer.json" ]; then
     # run the composer scripts for post-deploy
-    COMPOSER_CMD="${PHP_DIR}/bin/php \
+    COMPOSER_CMD="env PATH=${PATH}:${PHP_DIR}/bin ${PHP_DIR}/bin/php \
     /usr/local/bin/composer \
     --no-ansi"
     if su www-data -c "${COMPOSER_CMD} run-script -l" \
