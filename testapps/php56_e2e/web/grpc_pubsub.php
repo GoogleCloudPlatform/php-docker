72456d6f6d9a4798964707d1dca1c1a6a2fbac32
deustis <david.eustis@gmail.com>
2016-10-17 15:20:40 -0700

    Add failure message to grpc_pubsub.php

--- testapps/php56_e2e/web/grpc_pubsub.php
+++ testapps/php56_e2e/web/grpc_pubsub.php
@@ -4,44 +4,23 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 use Google\Auth\ApplicationDefaultCredentials;
 
-syslog(LOG_INFO, 'A');
-
 $auth_credentials = ApplicationDefaultCredentials::getCredentials(
     ['https://www.googleapis.com/auth/pubsub']
 );
 
-syslog(LOG_INFO, 'B');
-
 $channel_credentials = Grpc\ChannelCredentials::createSsl(
     file_get_contents(__DIR__ . '/../vendor/grpc/grpc/etc/roots.pem'));
-
-syslog(LOG_INFO, 'C');
 
 $opts = ['credentials' => $channel_credentials,
          'grpc.ssl_target_name_override' => 'pubsub.googleapis.com'];
 
-syslog(LOG_INFO, 'D');
-
 $client = new google\pubsub\v1\PublisherClient('pubsub.googleapis.com', $opts);
-
-syslog(LOG_INFO, 'E');
 
 $deadline = Grpc\Timeval::InfFuture();
 
-syslog(LOG_INFO, 'F');
-
 $req = new google\pubsub\v1\ListTopicsRequest();
-
-syslog(LOG_INFO, 'G');
-
 $proj = getenv('GAE_LONG_APP_ID');
-
-syslog(LOG_INFO, 'H');
-
 $req->setProject('projects/' . $proj);
-
-syslog(LOG_INFO, 'I');
-
 $call = $client->ListTopics(
     $req,
     [],
@@ -50,12 +29,13 @@ $call = $client->ListTopics(
          return $auth_credentials->updateMetadata([], $context->service_url);
      }]);
 
-syslog(LOG_INFO, 'J');
-
 list($response, $status) = $call->wait();
-
-syslog(LOG_INFO, 'K');
+if ($status->code != 0) {
+   echo 'gRPC to PubSub failed, printing RPC status:<br>';
+   print_r($status);
+   exit(1);
+}
 
 foreach ($response->getTopics() as $topic) {
-    echo $topic->getName() . "\n";
+    echo $topic->getName() . '<br>';
 }
