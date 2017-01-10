36691d7cb6d930ee815589587e85072ebce53a2e
Takashi Matsuo <tmatsuo@google.com>
2017-01-10 10:49:03 -0800

    Verify the composer's installer (#181)

--- php-nginx/build-scripts/build_php56.sh
+++ php-nginx/build-scripts/build_php56.sh
@@ -63,7 +63,20 @@ ${PHP56_DIR}/bin/pecl install grpc
 rm -rf /tmp/pear
 
 # Install composer
-curl -sS https://getcomposer.org/installer | \
-    ${PHP56_DIR}/bin/php -- \
+EXPECTED_SIGNATURE=$(curl -f https://composer.github.io/installer.sig)
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+ACTUAL_SIGNATURE=$(php -r "echo hash_file('SHA384', 'composer-setup.php');")
+
+if [ "$EXPECTED_SIGNATURE" != "$ACTUAL_SIGNATURE" ]
+then
+    >&2 echo 'ERROR: Invalid composer installer signature'
+    rm composer-setup.php
+    exit 1
+fi
+
+php composer-setup.php \
+    --quiet \
     --install-dir=/usr/local/bin \
     --filename=composer
+
+rm composer-setup.php
