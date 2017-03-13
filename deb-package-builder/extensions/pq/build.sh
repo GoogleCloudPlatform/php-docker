2ff0c1141e5a1a78eddab5eb6aceb976539aa818
Jeff Ching <ching.jeff@gmail.com>
2017-03-13 16:24:51 -0700

    Add ev, event, oauth, pq, rdkafka extensions (#230)
    * Add download_from_tarball function to the extension builders.
    
    If an extension's source is not available from PECL, you can use this
    function to download and unzip the tarball from an arbitrary url.
    
    Usage:
    download_from_tarball <url> <version>
    downlaod_from_tarball https://github.com/phalcon/cphalcon/archive/v3.0.4.tar.gz 3.0.4
    
    * Can build the phalcon PHP extension
    
    * Add ability to specify which extensions you wish to build.
    
    The parameter after the comma separated list of php versions can be a
    comma separated list of extension names you wish to build.
    
    Also, we copy the .deb packages out of the working directory into the
    artifact directory instead of working in the artifact directory. We also
    will not rebuild the php package if it already exists in the artifact
    directory.
    
    Fix README
    
    * Fix handling of beta packages from PECL
    
    * Add ev, event, oauth, pq, raphf, rdkafka
    
    * Fix default order of extension building. pq depends on raphf.
    
    Add ev, event, oauth, pq, raphf, rdkafka extensions to README.
    
    * Fix apcu_bc name
    
    * Fix jsonc package name
    
    * Fix upstream contact for raphf extension
    
    * Fix maintainer author for debian packages

---
+++ deb-package-builder/extensions/pq/build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -ex
+
+source ${DEB_BUILDER_DIR}/extensions/functions.sh
+
+echo "Building pq for gcp-php${SHORT_VERSION}"
+
+apt-get install -y libpq-dev
+dpkg -i ${BUILD_DIR}/gcp-php${SHORT_VERSION}-raphf*.deb
+/opt/php${SHORT_VERSION}/bin/php${SHORT_VERSION}-enmod raphf
+
+PNAME="gcp-php${SHORT_VERSION}-pq"
+
+# Download the source
+if [ ${SHORT_VERSION} == "56" ]; then
+    download_from_pecl pq 1.1.1
+else
+    download_from_pecl pq
+fi
+
+build_package pq
