b6e1e5282e74e6a6bd1a8000ff291e9dac4ed33d
Jeff Ching <chingor@google.com>
2017-05-09 11:34:08 -0700

    Build all images in parallel

--- scripts/build_images.sh
+++ scripts/build_images.sh
@@ -34,46 +34,17 @@ export RUNTIME_DISTRIBUTION
 SRC_TMP=$(mktemp -d)
 export PHP_BASE_IMAGE="gcr.io/${GOOGLE_PROJECT_ID}/php-base:${TAG}"
 export BASE_IMAGE="gcr.io/${GOOGLE_PROJECT_ID}/php:${TAG}"
-export TEST_RUNNER="gcr.io/${GOOGLE_PROJECT_ID}/php-test-runner:${TAG}"
-gcloud -q container builds submit --tag "${TEST_RUNNER}" \
-    cloudbuild-test-runner
 
-build_image () {
-    if [ "$#" -ne 2 ]; then
-        echo "Two arguments; the image name and the dir are required"
-        exit 1
-    fi
-    DIR="${2}"
-    # Build the image with container builder service if we have
-    # credentials.
-    export IMAGE="gcr.io/${GOOGLE_PROJECT_ID}/${1}:${TAG}"
-    SRC_DIR="${SRC_TMP}/${DIR}"
-    mkdir -p $(dirname ${SRC_DIR})
-    cp -R "${DIR}" "${SRC_DIR}"
-    # Replace the FROM line to point to our image in gcr.io.
-    if [ -f "${SRC_DIR}/Dockerfile.in" ]; then
-        envsubst '${BASE_IMAGE} ${PHP_BASE_IMAGE}' \
-                 < "${SRC_DIR}/Dockerfile.in" \
-                 > "${SRC_DIR}/Dockerfile"
-    fi
-    envsubst < "${SRC_DIR}/cloudbuild.yaml.in" > "${SRC_DIR}/cloudbuild.test.yaml"
-    gcloud -q container builds submit "${SRC_DIR}" \
-      --config "${SRC_DIR}"/cloudbuild.test.yaml --timeout 3600
-}
-
-build_image php-base php-base
-build_image php php-onbuild
-build_image php/gen-dockerfile builder/gen-dockerfile
-
-for TEMPLATE in `ls testapps/**/Dockerfile.in`
+for TEMPLATE in `find . -name Dockerfile.in`
 do
   envsubst '${BASE_IMAGE}' < ${TEMPLATE} > $(dirname ${TEMPLATE})/$(basename -s .in ${TEMPLATE})
 done
+envsubst '${BASE_IMAGE} ${PHP_BASE_IMAGE}' < php-onbuild/Dockerfile.in > php-onbuild/Dockerfile
 
-gcloud container builds submit testapps \
-  --config testapps/cloudbuild.yaml \
+gcloud container builds submit . \
+  --config cloudbuild.yaml \
   --timeout 3600 \
-  --substitutions _TAG=$TAG
+  --substitutions _TAG=$TAG,_RUNTIME_DISTRIBUTION=$RUNTIME_DISTRIBUTION
 
 if [ -z "${RUN_E2E_TESTS}" ]; then
     echo 'E2E test skipped'
