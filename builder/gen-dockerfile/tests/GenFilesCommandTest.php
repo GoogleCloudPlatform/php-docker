c3468b201f1e7d886ca73aa3c10270bf801a52de
Takashi Matsuo <tmatsuo@google.com>
2017-05-09 17:56:55 -0700

    Let the builder add env vars. (#274)
    * Let the builder add env vars.
    
    * Our BUILDER runs the composer script
    * env_variables in app.yaml will be in the final Dockerfile
    * some values in runtime_config will be transformed to an envvar
    * values in runtime_config always win
    
    * CS fix
    
    * Added a newline

--- builder/gen-dockerfile/tests/GenFilesCommandTest.php
+++ builder/gen-dockerfile/tests/GenFilesCommandTest.php
@@ -14,9 +14,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-namespace Google\Cloud\tests;
+namespace Google\Cloud\Runtimes\Builder;
 
-class GenFilesTest extends \PHPUnit_Framework_TestCase
+class GenFilesCommandTest extends \PHPUnit_Framework_TestCase
 {
     public static $testDir;
 
@@ -53,13 +53,14 @@ class GenFilesTest extends \PHPUnit_Framework_TestCase
     /**
      * @dataProvider dataProvider
      */
-    public function testGenFiles(
+    public function testGenFilesCommand(
         $dir,
         $baseImage,
         $appYamlEnv,
         $expectedDocRoot,
         $expectedDockerIgnore,
-        $expectedFrom
+        $expectedFrom,
+        $otherExpectations = []
     ) {
         // Copy all the files to the test dir
         $files = array('app.yaml', 'my.yaml', 'Dockerfile', '.dockerignore');
@@ -71,13 +72,13 @@ class GenFilesTest extends \PHPUnit_Framework_TestCase
         if (!empty($appYamlEnv)) {
             putenv('GAE_APPLICATION_YAML_PATH=' . $appYamlEnv);
         }
-        $genFiles = new \GenFiles(self::$testDir);
+        $genFiles = new GenFilesCommand(self::$testDir);
         $genFiles->createDockerfile($baseImage);
 
         $dockerfile = file_get_contents(self::$testDir . '/Dockerfile');
         $this->assertTrue($dockerfile !== false, 'Dockerfile should exist');
         $this->assertContains(
-            'ENV DOCUMENT_ROOT ' . $expectedDocRoot,
+            'DOCUMENT_ROOT=' . $expectedDocRoot,
             $dockerfile
         );
         $this->assertContains('FROM ' . $expectedFrom, $dockerfile);
@@ -91,6 +92,9 @@ class GenFilesTest extends \PHPUnit_Framework_TestCase
             $expectedDockerIgnore,
             $dockerignore
         );
+        foreach ($otherExpectations as $expectation) {
+            $this->assertContains($expectation, $dockerfile);
+        }
     }
 
     public function dataProvider()
@@ -103,7 +107,38 @@ class GenFilesTest extends \PHPUnit_Framework_TestCase
                 '',
                 '/app',
                 'added by the php runtime builder',
-                'gcr.io/google-appengine/php:latest'
+                'gcr.io/google-appengine/php-base:latest'
+                ["GOOGLE_RUNTIME_RUN_COMPOSER_SCRIPT=true \\\n"]
+            ],
+            [
+                // whitelist_functions
+                __DIR__ . '/test_data/whitelist_functions',
+                '',
+                '',
+                '/app',
+                'added by the php runtime builder',
+                'gcr.io/google-appengine/php-base:latest',
+                ["WHITELIST_FUNCTIONS=exec \\\n"]
+            ],
+            [
+                // whitelist_functions on env_variables
+                __DIR__ . '/test_data/whitelist_functions_on_env',
+                '',
+                '',
+                '/app',
+                'added by the php runtime builder',
+                'gcr.io/google-appengine/php-base:latest',
+                ["WHITELIST_FUNCTIONS=exec \\\n"]
+            ],
+            [
+                // whitelist_functions runtime_config wins
+                __DIR__ . '/test_data/whitelist_functions_on_both',
+                '',
+                '',
+                '/app',
+                'added by the php runtime builder',
+                'gcr.io/google-appengine/php-base:latest',
+                ["WHITELIST_FUNCTIONS=exec \\\n"]
             ],
             [
                 // Different yaml path
@@ -112,7 +147,7 @@ class GenFilesTest extends \PHPUnit_Framework_TestCase
                 'my.yaml',
                 '/app',
                 'added by the php runtime builder',
-                'gcr.io/google-appengine/php:latest'
+                'gcr.io/google-appengine/php-base:latest'
             ],
             [
                 // Overrides baseImage
@@ -130,7 +165,25 @@ class GenFilesTest extends \PHPUnit_Framework_TestCase
                 '',
                 '/app/web',
                 'added by the php runtime builder',
-                'gcr.io/google-appengine/php:latest'
+                'gcr.io/google-appengine/php-base:latest'
+            ],
+            [
+                // Has document_root set in env_variables
+                __DIR__ . '/test_data/docroot_env',
+                '',
+                '',
+                '/app/web',
+                'added by the php runtime builder',
+                'gcr.io/google-appengine/php-base:latest'
+            ],
+            [
+                // document_root in runtime_config wins
+                __DIR__ . '/test_data/docroot_on_both',
+                '',
+                '',
+                '/app/web',
+                'added by the php runtime builder',
+                'gcr.io/google-appengine/php-base:latest'
             ],
             [
                 // Has files already
