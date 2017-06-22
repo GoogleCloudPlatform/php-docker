852454d94e2b78beb071e1687828a4bdb713fd64
Jeff Ching <chingor@google.com>
2017-06-22 10:47:37 -0700

    build protobuf package from github release until published on pecl

--- deb-package-builder/extensions/protobuf/build.sh
+++ deb-package-builder/extensions/protobuf/build.sh
@@ -9,6 +9,10 @@ echo "Building protobuf for gcp-php${SHORT_VERSION}"
 PNAME="gcp-php${SHORT_VERSION}-protobuf"
 
 # Download the source
-download_from_pecl protobuf
+# download_from_pecl protobuf
+# build_package protobuf
 
+download_from_tarball https://github.com/google/protobuf/archive/v3.3.2.tar.gz 3.3.2
+
+PACKAGE_DIR=${PACKAGE_DIR}/php/ext/google/protobuf
 build_package protobuf
