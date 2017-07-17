c25645ac6769f8bf638a4250f6f8731bbf7ffb26
Takashi Matsuo <tmatsuo@google.com>
2017-07-17 11:02:42 -0700

    Stop compiling the trace extension on PHP 5.6

--- deb-package-builder/extensions/stackdriver_trace/build.sh
+++ deb-package-builder/extensions/stackdriver_trace/build.sh
@@ -9,8 +9,11 @@ echo "Building stackdriver trace for gcp-php${SHORT_VERSION}"
 PNAME="gcp-php${SHORT_VERSION}-stackdriver-trace"
 EXT_VERSION="0.1.0"
 
+if [ ${SHORT_VERSION} == '56' ]; then
+    echo "PHP 5.6 is not supported"
+    exit 0
+fi
+
 download_from_tarball https://github.com/GoogleCloudPlatform/stackdriver-trace-php-extension/archive/master.tar.gz ${EXT_VERSION}
 
-pwd
-ls -al
 build_package stackdriver_trace
