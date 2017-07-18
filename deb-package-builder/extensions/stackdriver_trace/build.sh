baf1770dd6da33185866f6ad88540e6600dc043b
Jeff Ching <ching.jeff@gmail.com>
2017-07-18 15:01:53 -0700

    Build stackdriver_trace extension from a GitHub release version archive. (#336)

--- deb-package-builder/extensions/stackdriver_trace/build.sh
+++ deb-package-builder/extensions/stackdriver_trace/build.sh
@@ -7,13 +7,13 @@ source ${DEB_BUILDER_DIR}/extensions/functions.sh
 echo "Building stackdriver trace for gcp-php${SHORT_VERSION}"
 
 PNAME="gcp-php${SHORT_VERSION}-stackdriver-trace"
-EXT_VERSION="0.1.0"
+EXT_VERSION="0.1.1"
 
 if [ ${SHORT_VERSION} == '56' ]; then
     echo "PHP 5.6 is not supported"
     exit 0
 fi
 
-download_from_tarball https://github.com/GoogleCloudPlatform/stackdriver-trace-php-extension/archive/master.tar.gz ${EXT_VERSION}
+download_from_tarball https://github.com/GoogleCloudPlatform/stackdriver-trace-php-extension/archive/v${EXT_VERSION}.tar.gz ${EXT_VERSION}
 
 build_package stackdriver_trace
