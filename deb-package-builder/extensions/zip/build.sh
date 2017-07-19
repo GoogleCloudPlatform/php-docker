ccc69fa9608d6372be8a5fd97dfb85966a1f3d35
Jeff Ching <chingor@google.com>
2017-07-19 13:39:41 -0700

    Add zip extension

---
+++ deb-package-builder/extensions/zip/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -ex
+source ${DEB_BUILDER_DIR}/extensions/functions.sh
+
+echo "Building ev for gcp-php${SHORT_VERSION}"
+
+PNAME="gcp-php${SHORT_VERSION}-zip"
+
+apt-get install -y libzip-dev
+
+# Download the source
+download_from_pecl zip
+
+build_package zip
