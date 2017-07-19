ea38adf3e79a8c2ebdacd76e56fcbad2be7b2a74
Jeff Ching <chingor@google.com>
2017-07-19 11:11:27 -0700

    add couchbase

---
+++ deb-package-builder/extensions/couchbase/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -ex
+source ${DEB_BUILDER_DIR}/extensions/functions.sh
+
+echo "Building ev for gcp-php${SHORT_VERSION}"
+
+PNAME="gcp-php${SHORT_VERSION}-couchbase"
+
+curl http://packages.couchbase.com/releases/couchbase-release/couchbase-release-1.0-3-amd64.deb -o couchbase-release-1.0-3-amd64.deb
+dpkg -i couchbase-release-1.0-3-amd64.deb
+
+apt-get update
+apt-get install -y libcouchbase-dev
+
+# Download the source
+download_from_pecl couchbase
+
+build_package couchbase
