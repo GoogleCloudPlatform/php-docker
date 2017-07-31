0b92465975e8d5d1fb0251c12472ab46a3f43334
Jeff Ching <chingor@google.com>
2017-07-31 14:18:34 -0700

    Use version 1.0.0 of lua extension for PHP 5.6

--- deb-package-builder/extensions/lua/build.sh
+++ deb-package-builder/extensions/lua/build.sh
@@ -12,6 +12,10 @@ ln -s /usr/include/lua5.3 /usr/include/lua
 ln -s /usr/lib/x86_64-linux-gnu/liblua5.3.a /usr/lib/x86_64-linux-gnu/lua.a
 
 # Download the source
-download_from_pecl lua
+if [ ${SHORT_VERSION} == '56' ]; then
+    download_from_pecl lua 1.0.0
+else
+    download_from_pecl lua
+fi
 
 build_package lua
