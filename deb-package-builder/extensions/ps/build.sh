373ca8c2c1b77b40dbe3eda5bd5b88656310e7bd
Jeff Ching <chingor@google.com>
2017-07-31 10:31:59 -0700

    Add ps extension

---
+++ deb-package-builder/extensions/ps/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -ex
+source ${DEB_BUILDER_DIR}/extensions/functions.sh
+
+echo "Building ev for gcp-php${SHORT_VERSION}"
+
+PNAME="gcp-php${SHORT_VERSION}-ps"
+
+apt-get install -y pslib-dev
+
+# Download the source
+if [ ${SHORT_VERSION} == '56' ]; then
+    download_from_pecl ps 1.4.0
+else
+    download_from_pecl ps
+fi
+
+build_package ps
