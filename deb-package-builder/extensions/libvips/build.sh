5a0dd8b681837bf26d0cd0548af5f6ad91bae4be
Jeff Ching <chingor@google.com>
2017-07-31 17:08:34 -0700

    Add libvips and vips

---
+++ deb-package-builder/extensions/libvips/build.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -ex
+
+source ${DEB_BUILDER_DIR}/extensions/functions.sh
+
+echo "Building libvips cpp driver"
+
+PNAME="libvips"
+VERSION="8.5.7"
+
+OUTPUT_FILE="${PNAME}_${VERSION}-1~gcp8+1_amd64.deb"
+
+apt-get install -y libtiff5-dev libjpeg62-turbo-dev libgsf-1-dev
+
+if [ ! -f "${ARTIFACT_DIR}/${OUTPUT_FILE}" ]; then
+    # Download the source
+    download_from_tarball https://github.com/jcupitt/libvips/releases/download/v${VERSION}/vips-${VERSION}.tar.gz ${VERSION}
+
+    cp -R ${DEB_BUILDER_DIR}/extensions/libvips/debian ${PACKAGE_DIR}
+
+    chmod +x ${PACKAGE_DIR}/debian/rules
+
+    pushd ${PACKAGE_DIR}
+    dch --create -v "${VERSION}" \
+        --package ${PNAME} --empty -M \
+        "Build ${VERSION} of ${PNAME}"
+    dpkg-buildpackage -us -uc -j"$(nproc)"
+    cp ../*.deb ${ARTIFACT_DIR}
+    popd
+fi
