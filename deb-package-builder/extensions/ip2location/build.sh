b8ba00bb729ecb82a51002286c87c9a0f90fd0d6
Jeff Ching <chingor@google.com>
2017-12-12 11:29:11 -0800

    Add ip2location extension

---
+++ deb-package-builder/extensions/ip2location/build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -ex
+source ${DEB_BUILDER_DIR}/functions.sh
+
+echo "Building ip2location for gcp-php${SHORT_VERSION}"
+
+PNAME="gcp-php${SHORT_VERSION}-ip2location"
+
+# Download the source
+download_from_pecl ip2location
+
+build_package ip2location
