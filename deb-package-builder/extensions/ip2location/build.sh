bb4dd0b9b3f848041c21b28dce1b8caa822f981e
Jeff Ching <chingor@google.com>
2017-12-12 14:55:33 -0800

    Build libip2location shared library as well

--- deb-package-builder/extensions/ip2location/build.sh
+++ deb-package-builder/extensions/ip2location/build.sh
@@ -6,6 +6,9 @@ echo "Building ip2location for gcp-php${SHORT_VERSION}"
 
 PNAME="gcp-php${SHORT_VERSION}-ip2location"
 
+install_last_package "libip2location"
+install_last_package "libip2location-dev"
+
 # Download the source
 download_from_pecl ip2location
 
