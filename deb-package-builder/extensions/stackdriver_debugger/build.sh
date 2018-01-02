ca2633df114b427493bc17038a6dc3c5a5827cca
Takashi Matsuo <tmatsuo@google.com>
2018-01-02 12:21:06 -0800

    Install stackdriver_debugger alpha version (#404)

--- deb-package-builder/extensions/stackdriver_debugger/build.sh
+++ deb-package-builder/extensions/stackdriver_debugger/build.sh
@@ -5,13 +5,12 @@ source ${DEB_BUILDER_DIR}/functions.sh
 echo "Building stackdriver-debugger for gcp-php${SHORT_VERSION}"
 
 PNAME="gcp-php${SHORT_VERSION}-stackdriver-debugger"
-EXT_VERSION="0.1.0"
 
 if [ ${SHORT_VERSION} == '56' ]; then
     echo "PHP 5.6 is not supported"
     exit 0
 fi
 
-download_from_pecl stackdriver_debugger-devel
+download_from_pecl stackdriver_debugger-alpha
 
 build_package stackdriver_debugger
