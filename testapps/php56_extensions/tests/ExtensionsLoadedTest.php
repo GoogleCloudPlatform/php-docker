3b82f180689435331ee5cc6a20f970c15a2209f8
Jeff Ching <ching.jeff@gmail.com>
2018-01-03 14:28:24 -0800

    Enable stackdriver_debugger, bitset, igbinary extensions (#405)
    * Enable stackdriver_debugger, bitset, igbinary in the extension enabler
    
    * Adding extension tests for bitset, igbinary for extension enabler
    
    * stackdriver debugger package name is hyphenated

--- testapps/php56_extensions/tests/ExtensionsLoadedTest.php
+++ testapps/php56_extensions/tests/ExtensionsLoadedTest.php
@@ -30,8 +30,10 @@ class ExtensionsLoadedTest extends TestCase
     {
         return [
             ['amqp'],
+            ['bitset'],
             ['eio'],
             ['hprose'],
+            ['igbinary'],
             ['jsond'],
             ['krb5'],
             ['lzf'],
