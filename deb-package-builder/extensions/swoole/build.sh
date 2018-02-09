aebd11995b13cd10edb978e9734a3bfbc0f848fa
Jeff Ching <ching.jeff@gmail.com>
2018-02-09 18:29:31 +0000

    Lock swoole version at 2.0.12 for php 5.6. 2.1.0 does not compile on 5.6 (#415)

--- deb-package-builder/extensions/swoole/build.sh
+++ deb-package-builder/extensions/swoole/build.sh
@@ -7,6 +7,10 @@ echo "Building swoole for gcp-php${SHORT_VERSION}"
 PNAME="gcp-php${SHORT_VERSION}-swoole"
 
 # Download the source
-download_from_pecl swoole
+if [ ${SHORT_VERSION} == "56" ]; then
+    download_from_pecl swoole 2.0.12
+else
+    download_from_pecl swoole
+fi
 
 build_package swoole
