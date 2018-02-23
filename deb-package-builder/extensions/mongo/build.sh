c058cf66a7444d6f8725c419858b706183672235
Jeff Ching <chingor@google.com>
2018-02-23 14:01:11 -0800

    Fix version check for building mongo

--- deb-package-builder/extensions/mongo/build.sh
+++ deb-package-builder/extensions/mongo/build.sh
@@ -7,7 +7,7 @@ echo "Building mongo for gcp-php${SHORT_VERSION}"
 PNAME="gcp-php${SHORT_VERSION}-mongo"
 
 # Download the source
-if [ ${SHORT_VERSION} != '5.6' ]; then
+if [ ${SHORT_VERSION} != '56' ]; then
     echo "deprecated mongo ext supported only on PHP 5.6"
     exit 0
 else
