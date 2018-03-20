517500fa96dafd98f9c93dc3f4cc071a458e0157
Jeff Ching <ching.jeff@gmail.com>
2018-03-20 14:37:09 -0700

    lock redis extension to 3.1.6. 4.0.0 has breaking changes (#424)

--- deb-package-builder/extensions/redis/build.sh
+++ deb-package-builder/extensions/redis/build.sh
@@ -9,6 +9,6 @@ echo "Building redis for gcp-php${SHORT_VERSION}"
 PNAME="gcp-php${SHORT_VERSION}-redis"
 
 # Download the source
-download_from_pecl redis
+download_from_pecl redis 3.1.6
 
 build_package redis
