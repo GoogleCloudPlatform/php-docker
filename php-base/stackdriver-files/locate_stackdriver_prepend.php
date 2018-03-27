ce835237baba58d91b46dd3377aa3b03cbb78945
Jeff Ching <chingor@google.com>
2018-03-27 10:38:38 -0700

    Fix prepend.php filename

--- php-base/stackdriver-files/locate_stackdriver_prepend.php
+++ php-base/stackdriver-files/locate_stackdriver_prepend.php
@@ -29,7 +29,7 @@ if (class_exists(Bootstrap::class)) {
         echo Bootstrap::prependFileLocation() . PHP_EOL;
     } else {
         // default to same directory as the Bootstrap.php
-        echo realpath(dirname($reflection->getFileName()) . '/bootstrap.php') . PHP_EOL;
+        echo realpath(dirname($reflection->getFileName()) . '/prepend.php') . PHP_EOL;
     }
 } else {
     die('cannot find ErrorReporting\Bootstrap class');
