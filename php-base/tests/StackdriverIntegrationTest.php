2d918cbf711e991655143ada44f827c7a66d66bc
Jeff Ching <chingor@google.com>
2018-04-18 12:57:02 -0700

    Add base tests for stackdriver integration

---
+++ php-base/tests/StackdriverIntegrationTest.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Copyright 2018 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+use PHPUnit\Framework\TestCase;
+
+class StackdriverIntegrationTest extends TestCase
+{
+    private $oldpwd;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->oldpwd = getcwd();
+    }
+
+    public function tearDown()
+    {
+        chdir($this->oldpwd);
+        parent::tearDown();
+    }
+
+    /**
+     * @dataProvider dataProvider
+     */
+    public function testEnabler($directory)
+    {
+        $dir = realpath(__DIR__ . '/' . $directory);
+        exec("cd $dir && composer install");
+        exec("php enable_stackdriver_prepend.php -a $dir");
+    }
+
+    public function dataProvider(){
+        return [
+            ['samples/stackdriver_individual']
+        ];
+    }
+}
