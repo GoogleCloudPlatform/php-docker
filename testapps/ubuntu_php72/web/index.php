99fb54aabeec1d760bdfe5797d40b22fa65135cb
Takashi Matsuo <tmatsuo@google.com>
2018-05-08 15:57:28 -0700

    Fixed the copyright headers, removed unnecessary bootstrap file

--- testapps/ubuntu_php72/web/index.php
+++ testapps/ubuntu_php72/web/index.php
@@ -1,7 +1,6 @@
 <?php
-
 /*
- * Copyright 2015 Google Inc.
+ * Copyright 2018 Google Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,6 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $app = new Silex\Application();
