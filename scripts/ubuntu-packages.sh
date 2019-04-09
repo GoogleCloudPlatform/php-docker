6ac892bc684ea2294e9be807c5d9ec196b7a47b4
Don McCasland <donmccasland@google.com>
2019-04-09 09:35:44 -0700

    Correcting paths for build scripts

--- scripts/ubuntu-packages.sh
+++ scripts/ubuntu-packages.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -ex
-source $KOKORO_PIPER_DIR/google3/third_party/runtimes_common/kokoro/common.sh
+export KOKORO_GITHUB_DIR=${KOKORO_ROOT}/src/github
+source ${KOKORO_GFILE_DIR}/kokoro/common.sh
+
 gcloud -q components update beta
 
-cd $KOKORO_PIPER_DIR/google3/third_party/php_docker/package-builder
+cd ${KOKORO_GITHUB_DIR}/php_docker/package-builder
 
 ./build_packages.sh
