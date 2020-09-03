a6c12f8f54de1d9fb6aeaf26c3857d1e1ff35be5
Don McCasland <donmccasland@google.com>
2020-09-03 12:13:17 -0700

    Right.  The test works on what's in the /workspace, not from fetched images.

--- builder/gen-dockerfile/tests/GenFilesCommandTest.php
+++ builder/gen-dockerfile/tests/GenFilesCommandTest.php
@@ -76,8 +76,6 @@ class GenFilesCommandTest extends TestCase
         if ($baseImages === null) {
             $baseImages =
                 [
-                    '--php56-image' => 'gcr.io/google-appengine/php56:latest',
-                    '--php70-image' => 'gcr.io/google-appengine/php70:latest',
                     '--php71-image' => 'gcr.io/google-appengine/php71:latest',
                     '--php72-image' => 'gcr.io/google-appengine/php72:latest',
                     '--php73-image' => 'gcr.io/google-appengine/php73:latest',
@@ -187,32 +185,6 @@ class GenFilesCommandTest extends TestCase
                 '',
                 [],
                 '\\Google\\Cloud\\Runtimes\\Builder\\Exception\\InvalidComposerFlagsException'
-            ],
-            [
-                // PHP 5.6
-                __DIR__ . '/test_data/php56',
-                null,
-                '',
-                '/app',
-                'added by the php runtime builder',
-                'gcr.io/google-appengine/php56:latest',
-                ["COMPOSER_FLAGS='--no-dev --prefer-dist' \\\n",
-                 "FRONT_CONTROLLER_FILE='index.php' \\\n",
-                 "DETECTED_PHP_VERSION='5.6' \n"
-                ]
-            ],
-            [
-                // PHP 7.0
-                __DIR__ . '/test_data/php70',
-                null,
-                '',
-                '/app',
-                'added by the php runtime builder',
-                'gcr.io/google-appengine/php70:latest',
-                ["COMPOSER_FLAGS='--no-dev --prefer-dist' \\\n",
-                 "FRONT_CONTROLLER_FILE='index.php' \\\n",
-                 "DETECTED_PHP_VERSION='7.0' \n"
-                ]
             ],
             [
                 // PHP 7.1
